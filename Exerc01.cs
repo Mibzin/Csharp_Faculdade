1e023f7f3a28772de88e2dc668326c46e265aadb
Luiz Mateus Cavalcante <162809323+Mibzin@users.noreply.github.com>
2026-09-16 10:40:51 -0300

    01

---
+++ Exerc01.cs
@@ -0,0 +1,25 @@
+//Console.WriteLine("Hello, World!");
+
+
+//int a, b;
+
+//double x;
+
+
+//Console.WriteLine("Calculadora \n");
+
+//Console.WriteLine("\nDigite  o valor de a = ");
+
+//a = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("\nDigite o valor de b = ");
+//b = int.Parse(Console.ReadLine());
+//x = 10.5;
+//x = (int)x;
+//x = (double)a / b; //a e b viram valores flutuantes  (usamos casting)
+//Console.WriteLine("A = {0} e B = {1}", a, b);
+//Console.WriteLine("\nValor da soma  =  " + (a + b));
+//Console.WriteLine("\nValor da subtração = " + (a - b));
+//Console.WriteLine("\nValor da multiplicação = " + (a * b));
+//Console.WriteLine("\nValor da Divisão = " + x.ToString("0.00"));
+//Console.WriteLine("\nValor da Divisão = {0}", x.ToString("0.00"));
