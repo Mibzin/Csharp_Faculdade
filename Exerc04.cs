54cb6d0a7900b15e3b1455464c4c7535165cf87c
Luiz Mateus Cavalcante <162809323+Mibzin@users.noreply.github.com>
2026-09-16 10:43:54 -0300

    04

---
+++ Exerc04.cs
@@ -0,0 +1,20 @@
+//-----------4
+
+
+int a;
+int b;
+int resul;
+
+Console.WriteLine("Produtos Simples");
+
+Console.WriteLine("Digite o primeiro valor:  ");
+a = int.Parse(Console.ReadLine());
+// Console.WriteLine("O valor informado é : "+a);
+
+Console.WriteLine("Digite o segundo valor:  ");
+b = int.Parse(Console.ReadLine());
+// Console.WriteLine("O valor informado é : "+b);
+
+resul = a * b;
+
+Console.WriteLine("PROD =  " + resul);
