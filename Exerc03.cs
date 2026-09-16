5c78cbbe519c733b36dc98de7ec00f85a32bf329
Luiz Mateus Cavalcante <162809323+Mibzin@users.noreply.github.com>
2026-09-16 10:43:06 -0300

    03

---
+++ Exerc03.cs
@@ -0,0 +1,14 @@
+//-----------3
+
+int a;
+int b;
+int soma;
+
+Console.WriteLine("Digite o Valor de A ");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o Valor de B ");
+b = int.Parse(Console.ReadLine());
+
+soma = a + b;
+Console.WriteLine("SOMA = " + soma);
